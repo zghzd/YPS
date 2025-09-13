d2a7c4d889c225c0fcbe42e5d27f41c263627338
z z <zfg_0362@outlook.com>
2025-09-13 19:51:27 +0800

    基本文件

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+#include <vector>
+#include "_log.h"
+#include "base_io.h"
+#include "file_compare.h"
+#include "fileread_withkey.h"
+
+static std::vector<std::string> filecompare(std::string in_file_name1, std::string in_file_name2);
+int file_write_new(std::string file_name, std::string write_str);
+std::string file_read(std::string file_name);
+int file_write_c(std::string file_name, std::string write_str);
+std::string getConfigValue(const std::string& filename, const std::string& projectName);
+std::vector<std::string> _log(std::string log_type, std::string log_time, int log_code, std::string log_text);
 
 int main() {
-	std::cout << "0" << std::endl;
+	
 	return 0;
 }
