e8ebfc92cb73c103023e0a8b10172d15c842cb8d
z z <zfg_0362@outlook.com>
2025-09-19 22:08:18 +0800

    版权信息

---
+++ __copyright.h
@@ -0,0 +1,24 @@
+//（版权声明适用于整个解决方案/项目/存储库，除非部分文件中有明确说明）
+/*
+Copyright 2025 翊
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#include <iostream>
+std::string __copyright() {
+    std::string __copyright = "翊 的程序\nCopyright (c) 2025 翊\nLicensed under the Apache License, Version 2.0\n";
+    //如果你使用/更改了开放的源代码，请在下方_copyright写入字符串(注意使用\n换行符)。应当包含更改内容和开发者信息，同时，可以更改__ver。
+    std::string __ver = "0.1.13\n";
+    std::string _copyright = "";
+    return (__copyright + "版本:" + __ver + _copyright);
+}
