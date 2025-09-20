5b7121a5762b3b6237d82ba3099c890b78261850
z z <zfg_0362@outlook.com>
2025-09-20 21:30:13 +0800

    简易文件熵计算

---
+++ entropy.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "_log.h"
+double FileEntropy(const std::string& filename, std::string __data) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "无法打开文件: " << filename << std::endl;
+        _log(__data + ".log", "E", "0001-0000", "entropy求熵", "熵值计算失败，无法打开文件:" + filename);
+        return -1.0;
+    }
+    std::vector<size_t> freq(256, 0);
+    size_t totalBytes = 0;
+    unsigned char byte;
+
+    while (file.read(reinterpret_cast<char*>(&byte), 1)) {
+        ++freq[byte];
+        ++totalBytes;
+    }
+    file.close();
+    if (totalBytes == 0) return 0.0;
+    double entropy = 0.0;
+    for (size_t i = 0; i < freq.size(); ++i) {
+        if (freq[i] == 0) continue;
+        double p = static_cast<double>(freq[i]) / totalBytes;
+        entropy -= p * std::log2(p);
+    }
+    _log(__data + ".log", "D", "0000-0001", "entropy求熵", "完成");
+    return entropy;
+}
--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include "file_compare.h"
 #include "fileread_withkey.h"
 #include "opengl.h"
+#include "entropy.h"
 
 std::string __data = time_now(1);
 
 int main() {
 	_log(__data + ".log", "D", "0000-0001", "应用程序主线程", "应用程序成功启动且已完成基本的初始化");
-	//_opengl(__data);
+	
 	return 0;
 }
